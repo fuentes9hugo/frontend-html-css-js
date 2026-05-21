e3ee0ed21c3cd9a9b2a252e0b45de3f58095dcc4
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-05-21 22:21:53 +0200

    First interactions DOM/JS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*
+* JS for the entry form data checkup
+*/
+
+// Capture nick input value
+const nickInput = document.getElementById("nick");
+console.log(nickInput.nodeType);
+nickInput.value = "Paco";
+console.log(nickInput.value);
+
+// Capture select value
+const tamanoInput = document.getElementById("tamano");
+console.log(tamanoInput.value);
+console.log(tamanoInput.options[tamanoInput.selectedIndex].text);
