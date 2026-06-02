8c9e764b733a04409e5f69a38c47f11521cbbcdf
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-06-02 18:59:58 +0200

    Form error messages

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 const nickInput = document.getElementById("nick");
 const tamanoInput = document.getElementById("tamano");
 const formEntrada = document.getElementById("formEntrada");
+const error = document.getElementById("error");
 
 // Event functions
 function comprobarForm(event) {
@@ -15,11 +16,13 @@ function comprobarForm(event) {
         console.log("No hay nick");
         nickInput.focus();
         event.preventDefault();
+        error.innerText = "El campo de nick no puede estar vacío";
         return false;
     } else if (tamanoInput.value == "0") {
         console.log("No se ha seleccionado tamño del panel");
         tamanoInput.focus();
         event.preventDefault();
+        error.innerText = "Se debe seleccionar un tamaño de panel";
         return false;
     }
     return true;
