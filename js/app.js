5eb3d7f7dea25ed61c3a88702ca5a134d9b973a8
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-06-02 18:46:46 +0200

    Starting form check

--- js/app.js
+++ js/app.js
@@ -4,17 +4,26 @@
 */
 
 // Initialization of var, objects, DOM
-const botonJugar = document.getElementById("jugar");
+const nickInput = document.getElementById("nick");
+const tamanoInput = document.getElementById("tamano");
+const formEntrada = document.getElementById("formEntrada");
 
 // Event functions
-function comprobarForm() {
-    console.log("Comprobamos el formulario");
+function comprobarForm(event) {
+    // Check changes
+    if (nickInput.value.length == 0) {
+        console.log("No hay nick");
+        nickInput.focus();
+        event.preventDefault();
+        return false;
+    } else if (tamanoInput.value == "0") {
+        console.log("No se ha seleccionado tamño del panel");
+        tamanoInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    return true;
 }
 
-function ejecutarAccion() {
-    console.log("Ejecutar acción");
-    botonJugar.removeEventListener("click", ejecutarAccion);
-}
-
-botonJugar.addEventListener("click", comprobarForm);
-botonJugar.addEventListener("click", ejecutarAccion);
+// Events charge start
+formEntrada.addEventListener("submit", comprobarForm);
